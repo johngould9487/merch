ec63563c0b19724d17f0ab81f9060dcb1e4c63cc
John Gould <johngould9487@gmail.com>
2018-08-23 08:56:32 +0100

    added transaction functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'posters#index'
-  resources :posters
+  resources :posters do
+    resources :transactions, only: %i[show new create]
+  end
 end
 
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,7 @@
 
 puts "Create new db"
 
+Transaction.destroy_all
 Poster.destroy_all
 User.destroy_all
 
@@ -18,26 +19,26 @@ puts "Creating database..."
 user = User.create(email: "andy@andy.com", password: "123123")
 user = User.create(email: "alex@alex.com", password: "123123")
 
-Poster.create!(user: user, venue_name: "Apollo", artist_name: "James Blake", description: "Great gig, great poster, great band", price: 40, photo: "https://res.cloudinary.com/duspt82gb/image/upload/v1534947778/James_Blake.jpg", date: Time.now)
+Poster.create!(sold: false, user: user, venue_name: "Apollo", artist_name: "James Blake", description: "Great gig, great poster, great band", price: 40, photo: "https://res.cloudinary.com/duspt82gb/image/upload/v1534947778/James_Blake.jpg", date: Time.now)
 
-Poster.create!(user: user, venue_name: "The Roundhouse", artist_name: "Patrick Wolf", description: "Outstanding gig, you should have been there", price: 20, photo: "https://res.cloudinary.com/duspt82gb/image/upload/v1534947778/Patrick_Wolf.jpg", date: Time.now)
+Poster.create!(sold: false, user: user, venue_name: "The Roundhouse", artist_name: "Patrick Wolf", description: "Outstanding gig, you should have been there", price: 20, photo: "https://res.cloudinary.com/duspt82gb/image/upload/v1534947778/Patrick_Wolf.jpg", date: Time.now)
 
-Poster.create!(user: user, venue_name: "Barfly", artist_name: "Belle & Sebastien", description: "Great gig, great poster", price: 35, photo: "https://res.cloudinary.com/duspt82gb/image/upload/v1534947777/Belle_And_Sebastien.jpg", date: Time.now)
+Poster.create!(sold: false, user: user, venue_name: "Barfly", artist_name: "Belle & Sebastien", description: "Great gig, great poster", price: 35, photo: "https://res.cloudinary.com/duspt82gb/image/upload/v1534947777/Belle_And_Sebastien.jpg", date: Time.now)
 
-Poster.create!(user: user, venue_name: "Earls Court", artist_name: "Arcade Fire", description: "Great gig, great poster", price: 4, photo: "https://res.cloudinary.com/duspt82gb/image/upload/v1534947776/Arcade_Fire.jpg", date: Time.now)
+Poster.create!(sold: false, user: user, venue_name: "Earls Court", artist_name: "Arcade Fire", description: "Great gig, great poster", price: 4, photo: "https://res.cloudinary.com/duspt82gb/image/upload/v1534947776/Arcade_Fire.jpg", date: Time.now)
 
-Poster.create!(user: user, venue_name: "Water Rats", artist_name: "Bon Iver", description: "Great gig, great poster", price: 100, photo: "https://res.cloudinary.com/duspt82gb/image/upload/v1534947776/Bon_Iver.jpg", date: Time.now)
-
-
+Poster.create!(sold: false, user: user, venue_name: "Water Rats", artist_name: "Bon Iver", description: "Great gig, great poster", price: 100, photo: "https://res.cloudinary.com/duspt82gb/image/upload/v1534947776/Bon_Iver.jpg", date: Time.now)
 
 
-Poster.create!(user: user, venue_name: "Boston Harbour", artist_name: "Ratatat", description: "Great gig, great poster", price: 800, photo: "https://res.cloudinary.com/duspt82gb/image/upload/v1534949361/Ratatat.jpg", date: Time.now)
 
-Poster.create!(user: user, venue_name: "Wembley", artist_name: "Pink Floyd", description: "Great gig, great poster", price: 300, photo: "https://res.cloudinary.com/duspt82gb/image/upload/v1534949361/Pink_floyd.jpg", date: Time.now)
 
-Poster.create!(user: user, venue_name: "Brixton Academy", artist_name: "Peter Bjorn and John", description: "Great gig, great poster", price: 30, photo: "https://res.cloudinary.com/duspt82gb/image/upload/v1534949361/PBJ.jpg", date: Time.now)
+Poster.create!(sold: false, user: user, venue_name: "Boston Harbour", artist_name: "Ratatat", description: "Great gig, great poster", price: 800, photo: "https://res.cloudinary.com/duspt82gb/image/upload/v1534949361/Ratatat.jpg", date: Time.now)
 
-Poster.create!(user: user, venue_name: "Emirates", artist_name: "Black Keys", description: "Great gig, great poster", price: 700, photo: "https://res.cloudinary.com/duspt82gb/image/upload/v1534949360/Black_Keys.jpg", date: Time.now)
+Poster.create!(sold: false, user: user, venue_name: "Wembley", artist_name: "Pink Floyd", description: "Great gig, great poster", price: 300, photo: "https://res.cloudinary.com/duspt82gb/image/upload/v1534949361/Pink_floyd.jpg", date: Time.now)
+
+Poster.create!(sold: false, user: user, venue_name: "Brixton Academy", artist_name: "Peter Bjorn and John", description: "Great gig, great poster", price: 30, photo: "https://res.cloudinary.com/duspt82gb/image/upload/v1534949361/PBJ.jpg", date: Time.now)
+
+Poster.create!(sold: false, user: user, venue_name: "Emirates", artist_name: "Black Keys", description: "Great gig, great poster", price: 700, photo: "https://res.cloudinary.com/duspt82gb/image/upload/v1534949360/Black_Keys.jpg", date: Time.now)
 
 
 
